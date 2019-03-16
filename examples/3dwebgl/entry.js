7071d0a50337d60970044da086a1627efc6be7ad
xutuhao <46103791+xutuhao@users.noreply.github.com>
2019-03-16 15:28:04 +0800

    Add files via upload

---
+++ examples/3dwebgl/entry.js
@@ -0,0 +1,21 @@
+/* Copyright© 2000 - 2018 SuperMap Software Co.Ltd. All rights reserved.*/
+require.config({
+    paths: {
+        'Cesium': 'https://www.supermapol.com/earth/Build/Cesium/Cesium',
+        'Zlib': 'https://www.supermapol.com/earth/Build/Cesium/Workers/zlib.min'
+    },
+    shim: {
+        Cesium: {
+            exports: 'Cesium'
+        },
+        Zlib: {
+            exports: 'Zlib'
+        }
+    }
+});
+
+if (typeof Cesium !== "undefined" && typeof Zlib !== "undefined") {
+    onload(Cesium, Zlib);
+} else if (typeof require === "function") {
+    require(["Cesium", "Zlib"], onload);
+}
